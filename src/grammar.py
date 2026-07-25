105db7565e9955b604b70ceb0cb2a01804babade
David Cuenca <111570446+kuenk@users.noreply.github.com>
2026-07-25 16:18:43 +0200

    Add files via upload

--- src/grammar.py
+++ src/grammar.py
@@ -1,25 +1,42 @@
+import re
 
 
-
-def es_continuacion_valida(generado, candidato, opciones):
-    extendido = generado + candidato
-    for opcion in opciones:
-        if opcion.startswith(extendido):
+def enum_continue(generate, candidate, options):
+    extend = generate + candidate
+    for option in options:
+        if option.startswith(extend):
             return True
     return False
 
-def esta_completo(generado, opciones):
-    return generado in opciones
+def enum_complete(generate, options):
+    return generate in options
 
 
+def number_continue(generate, candidate):
+    extend = generate + candidate
+    if "-" in extend:
+        if extend.count("-") > 1:
+            return False
+        if extend.index("-") != 0:
+            return False
+    if extend.count(".") > 1:
+        return False
+    return True
 
 
-if generado.count(".") > 1:
+def number_complete(generate):
+    patron = re.compile(r"^-?\d+(\.\d+)?$")
+    return bool(patron.fullmatch(generate))
+
+
+def string_continue(candidate: str) -> bool:
+    if candidate == '"':
+        return  True
+    if any(character in candidate for character in ['"', "\n", "\t", "\r"]):
+        return False    
+    return True
+
+def string_complete(generate):
+    if generate.endswith('"'):
+        return True
     return False
-else:
-    return True
-
-if generado.index("-") == 0:
-    return True
-else
-    refurn False
